8d3bc74738fc2fe0fd15bd1245e4dac0a173537d
Pedro Javier <thepetermail@gmail.com>
2016-07-30 22:04:51 +0200

    1.2 Release
    This is the 1.2 beta release, with the following changes:
    -Removed raworiginal.s for copyright reasons
    -Added internalmem.dat so builds will work fine
    -Added autowrite/fix to file
    
    TODO:
    -Despite autowrite has been implemented, tool is not reloading file,
    therefore you will have to fix slot1/slot2 checksums, close the tool,
    reopen it and fix file checksum. If you don't close the tool before
    patching file checksum it will not be correct, therefore save will be
    marked as corrupt.

--- main.cpp
+++ main.cpp
@@ -1,26 +1,9 @@
-// Transformers-Checksum
+// TransformersChecksum.cpp : Defines the entry point for the console application.
 // REVISION 1.1
-
-/*
-Copyright (C) 2016 pedro-javierf
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
 
 #include <iostream> //I/O
 #include <fstream>  //File I/O
-#include <intrin.h> //Win32 only
+#include <intrin.h> //Win32 only, endian functions
 
 
 //By JohnScipione, Thanks!
@@ -38,50 +21,56 @@ unsigned int CalcSlot1Checksum(char * memblock2, char * r12mem);
 unsigned int CalcSlot2Checksum(char * memblock2, char * r12mem);
 unsigned int CalcFileChecksum(char * memblock2, char * r12mem);
 
+//lol it is working now! 0x0000BEEF is for global checksum!!
+
 int main(int argc, char* argv[])
 {
 	
 	//File object to read ROM from disk
-	std::ifstream reader;
-	//Open rom by its end (EOF) so we'll know it's exact size
-	reader.open("R12.dat", std::ios::in | std::ios::binary | std::ios::ate);
+	//Used to be an ifstrem but now it's a fstream to both write and read
+	std::ifstream InternalmemStream;
+	//Open file by its end (EOF) so we'll know it's exact size
+	InternalmemStream.open("internalmem.dat", std::ios::in | std::ios::binary | std::ios::ate);
 	//Check
-	if (!reader.is_open())
+	if (!InternalmemStream.is_open())
 	{
-		std::cout << " Can't Open R12.dat";
+		std::cout << "[!] Can't Open internalmem.dat";
 		exit(EXIT_FAILURE);
 	}
 
 	//Temporal buffer to store the rom before copying to emulated memory
 	char * r12mem;
 	//Get rom size
-	std::streampos size = reader.tellg();
-	std::cout << "R12 Size: " << size << " bytes" << std::endl;
+	std::streampos size = InternalmemStream.tellg();
+	std::cout << "[>] Internal Memory Size: " << size << " bytes" << std::endl;
 	//Allocate memory for it
 	r12mem = new char[size];
 	//Seek to the start of the file
-	reader.seekg(0, std::ios::beg);
+	InternalmemStream.seekg(0, std::ios::beg);
 	//Read the ROM
-	reader.read(r12mem, size);
-	std::cout << "R12 Loaded" << std::endl;
-
+	InternalmemStream.read(r12mem, size);
+	std::cout << "[>] Internal Memory Loaded" << std::endl;
+	InternalmemStream.close();
 	
 	
 
-	std::ifstream reader2;
-	reader2.open("Transformers.sav", std::ios::in | std::ios::binary | std::ios::ate);
-	if (!reader2.is_open())
+	std::fstream saveStream;
+	saveStream.open("Transformers.sav", std::ios::in | std::ios::out | std::ios::binary | std::ios::ate);
+	if (!saveStream.is_open())
 	{
-		std::cout << " Can't Open Transformers.sav";
+		std::cout << "[!] Can't Open Transformers.sav";
 		exit(EXIT_FAILURE);
 	}
 
 	char * memblock2;
-	std::streampos size2 = reader2.tellg();
-	std::cout << "Savegame Size: " << size2 << " bytes" << std::endl;
+	std::streampos size2 = saveStream.tellg();//Since we opened the file by it's end we can retrieve it's size
+	std::cout << "[>] Savegame Size: " << size2 << " bytes" << std::endl;
 	memblock2 = new char[size2];
-	reader2.seekg(0, std::ios::beg);
-	reader2.read(memblock2, size2);
+	saveStream.seekg(0, std::ios::beg);//The we just seek (move) to the begining of the file (0x0)
+	saveStream.read(memblock2, size2);
+	//We are not closing the stream since we'll use it to write 
+
+	unsigned int check1, check2, check3;
 
 	inicio:
 	system("cls");
@@ -89,23 +78,35 @@ int main(int argc, char* argv[])
 	std::cout << " ##### Transformers Checksum Tool #####" << std::endl << std::endl;
 	std::cout << "1. Calc Slot 1 checksum" << std::endl;
 	std::cout << "2. Calc Slot 2 checksum" << std::endl;
-	std::cout << "3. Calc File checksum" << std::endl << std::endl;
+	std::cout << "3. Calc File checksum" << std::endl;
+	std::cout << "4. Save changes and exit" << std::endl << std::endl;
 	std::cout << "> ";
 	std::cin >> opt; std::cout << std::endl;
 
 	switch (opt)
 	{
 	case 1:
-		CalcSlot1Checksum(memblock2, r12mem);
+		check1  = CalcSlot1Checksum(memblock2, r12mem);//Get checksum in little endian. Must be writen to file in big endian
+		saveStream.seekg(0xFB);//Checksum 1 pos
+		saveStream.write(reinterpret_cast<const char *>(&check1), sizeof(check1)); //Yep correct way of doing this
+		std::cout << check1;
+		std::cin.get();
 		std::cin.get();
 		break;
 	case 2:
-		CalcSlot2Checksum(memblock2, r12mem);
+		check2 = CalcSlot2Checksum(memblock2, r12mem);
+		saveStream.seekg(0x1E0);//Checksum 2 pos
+		saveStream.write(reinterpret_cast<const char *>(&check2), sizeof(check2)); //Yep correct way of doing this
 		std::cin.get();
 		break;
 	case 3:
-		CalcFileChecksum(memblock2, r12mem);
-		std::cin.get();
+		check3 = CalcFileChecksum(memblock2, r12mem);
+		saveStream.seekg(0x1FE);//Checksum 3 pos
+		saveStream.write(reinterpret_cast<const char *>(&check3), sizeof(check3)); //Yep correct way of doing this
+		break;
+	case 4:
+		saveStream.close();
+		exit(EXIT_SUCCESS);
 		break;
 	}
 	goto inicio;
@@ -119,7 +120,6 @@ int main(int argc, char* argv[])
 	//0x00000000 -> Slot 2 Checksum seed - Counter: 0xDF - r1  [.].!?New Game   OK  WE NEED TO POINT TO [] byte  0x100
 	//0X00000000 -> Slot 1 Checksum seed - Counter: 0xDF - r1  [.]'..AAAAAAAB   OK  WE NEED TO POINT TO [] byte  0x1C
 
-	return 0;
 	
 }
 
@@ -140,20 +140,18 @@ unsigned int CalcSlot1Checksum(char * memblock2, char * r12mem)
 
 		r3 = memblock2[r1]; r1++; //Where r1 is address where savedata is 0x0 for slot 1  and 0x00000100 for slot 2 ||THIS FAILS||
 
-		//THIS IS A LITTLE FIX//
-		//SOMETIME THE CODE ABOVE LOADS THE BYTE AT R3 LIKE 0xFFFFFFAA instead of 0x00000AA so this dirty patch will solve that
-		r3 = r3 << 0x18;
-		r3 = logicalRightShift(r3, 0x18);
-		//UPDATE WITH AND
+		//COOL PATCH TO CORRECTLY FIX/LOAD R3 ADDRESSES
+		r3 = r3 & 0x000000FF;
+
 
 
 		if (r2 == 0){ breaker = true; }
 		r2 = r2 - 1;
 
-		r3 = r3 ^ (arithmeticRightShift(r0, 0x8)); 
+		r3 = r3 ^ (arithmeticRightShift(r0, 0x8));  //GOOD UNTIL HERE
 
 	
-		r3 = r3 << 1;
+		r3 = r3 << 1;        //Yay! //GOOD UNTIL HERE
 
 
 
@@ -162,13 +160,13 @@ unsigned int CalcSlot1Checksum(char * memblock2, char * r12mem)
 			std::cout << "call to r12 mem very big: " << r3;
 			std::cin.get();
 		}
-		//gets byte at r3, merges it with byte at r3+1
-		r3 = ((unsigned char)r12mem[r3] << 8) | (unsigned char)r12mem[r3 + 1]; 
+		//YEEEEAH IT WORRKS! gets byte at r3, merges it with byte at r3+1
+		r3 = ((unsigned char)r12mem[r3] << 8) | (unsigned char)r12mem[r3 + 1]; //forget this -> It's ok but compiler fucks it when using [r3+1] In addition you have to use little endian bitch
 		r3 = _byteswap_ushort(r3); //Since nds is little endian and my PC uses big endian I'll convert it when reading from memory
-
+		//Unsigned int is 2 bytes -> 16 bits -> ushort
 
 		
-		r0 = r3 ^ (r0 << 0x8); 
+		r0 = r3 ^ (r0 << 0x8); //Yay! r3 = 0x00004084 this xor is the one that increments r0
 		
 		r0 = r0 << 0x10;
 		
@@ -176,10 +174,11 @@ unsigned int CalcSlot1Checksum(char * memblock2, char * r12mem)
 		
 
 		if (breaker == true){ break; }
+		//std::cin.get();
 	}
 	std::cout << "Slot 1 Checksum(r0): " << std::hex << r0 << std::endl;
 
-	std::cin.get();
+	//Actually checksum is returned as little endian
 	return r0;
 }
 
@@ -198,22 +197,20 @@ unsigned int CalcSlot2Checksum(char * memblock2, char * r12mem)
 	while (1) //Simulate loop to calculate crc
 	{
 
-		r3 = memblock2[r1]; r1++; //Where r1 is address where savedata is 0x0 for slot 1  and 0x00000100 for slot 2
+		r3 = memblock2[r1]; r1++; //Where r1 is address where savedata is 0x0 for slot 1  and 0x00000100 for slot 2 ||THIS FAILS||
 
-		//THIS IS A LITTLE FIX//
-		//SOMETIME THE CODE ABOVE LOADS THE BYTE AT R3 LIKE 0xFFFFFFAA instead of 0x00000AA so this dirty patch will solve that
-		r3 = r3 << 0x18;
-		r3 = logicalRightShift(r3, 0x18);
+		//COOL PATCH TO CORRECTLY FIX/LOAD R3 ADDRESSES
+		r3 = r3 & 0x000000FF;
 
 
 
 		if (r2 == 0){ breaker = true; }
 		r2 = r2 - 1;
 
-		r3 = r3 ^ (arithmeticRightShift(r0, 0x8));  
+		r3 = r3 ^ (arithmeticRightShift(r0, 0x8));  //GOOD UNTIL HERE
 
 
-		r3 = r3 << 1;   
+		r3 = r3 << 1;        //Yay! //GOOD UNTIL HERE
 
 
 
@@ -222,8 +219,8 @@ unsigned int CalcSlot2Checksum(char * memblock2, char * r12mem)
 			std::cout << "call to r12 mem very big: " << r3;
 			std::cin.get();
 		}
-		// gets byte at r3, merges it with byte at r3+1
-		r3 = ((unsigned char)r12mem[r3] << 8) | (unsigned char)r12mem[r3 + 1]; 
+		//YEEEEAH IT WORRKS! gets byte at r3, merges it with byte at r3+1
+		r3 = ((unsigned char)r12mem[r3] << 8) | (unsigned char)r12mem[r3 + 1]; //forget this -> It's ok but compiler fucks it when using [r3+1] In addition you have to use little endian bitch
 		r3 = _byteswap_ushort(r3); //Since nds is little endian and my PC uses big endian I'll convert it when reading from memory
 
 
@@ -259,22 +256,19 @@ unsigned int CalcFileChecksum(char * memblock2, char * r12mem)
 	while (1) //Simulate loop to calculate crc
 	{
 
-		r3 = memblock2[r1]; r1++; //Where r1 is address where savedata is 0x0 for slot 1  and 0x00000100 for slot 2 
+		r3 = memblock2[r1]; r1++; //Where r1 is address where savedata is 0x0 for slot 1  and 0x00000100 for slot 2 ||THIS FAILS||
 
-		//THIS IS A LITTLE FIX//
-		//SOMETIME THE CODE ABOVE LOADS THE BYTE AT R3 LIKE 0xFFFFFFAA instead of 0x00000AA so this dirty patch will solve that
-		r3 = r3 << 0x18;
-		r3 = logicalRightShift(r3, 0x18);
-
+		//COOL PATCH TO CORRECTLY FIX/LOAD R3 ADDRESSES
+		r3 = r3 & 0x000000FF;
 
 
 		if (r2 == 0){ breaker = true; }
 		r2 = r2 - 1;
 
-		r3 = r3 ^ (arithmeticRightShift(r0, 0x8));  
+		r3 = r3 ^ (arithmeticRightShift(r0, 0x8));  //GOOD UNTIL HERE
 
 
-		r3 = r3 << 1;        
+		r3 = r3 << 1;        //Yay! //GOOD UNTIL HERE
 
 
 
@@ -283,13 +277,13 @@ unsigned int CalcFileChecksum(char * memblock2, char * r12mem)
 			std::cout << "call to r12 mem very big: " << r3;
 			std::cin.get();
 		}
-		//gets byte at r3, merges it with byte at r3+1
-		r3 = ((unsigned char)r12mem[r3] << 8) | (unsigned char)r12mem[r3 + 1]; 
+		//YEEEEAH IT WORRKS! gets byte at r3, merges it with byte at r3+1
+		r3 = ((unsigned char)r12mem[r3] << 8) | (unsigned char)r12mem[r3 + 1]; //forget this -> It's ok but compiler fucks it when using [r3+1] In addition you have to use little endian bitch
 		r3 = _byteswap_ushort(r3); //Since nds is little endian and my PC uses big endian I'll convert it when reading from memory
 
 
 
-		r0 = r3 ^ (r0 << 0x8); 
+		r0 = r3 ^ (r0 << 0x8); //Yay! r3 = 0x00004084 this xor is the one that increments r0
 
 		r0 = r0 << 0x10;
 
@@ -297,6 +291,7 @@ unsigned int CalcFileChecksum(char * memblock2, char * r12mem)
 
 
 		if (breaker == true){ break; }
+		//std::cin.get();
 	}
 	std::cout << "File Checksum(r0): " << std::hex << r0 << std::endl;
 
