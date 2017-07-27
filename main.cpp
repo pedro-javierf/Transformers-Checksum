25408d2474c6316a8966775bc79e2d6c3bc37f58
Pedro Javier <pedro-javierf@users.noreply.github.com>
2017-07-27 16:20:26 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -150,7 +150,7 @@ unsigned int CalcSlot1Checksum(char * memblock2, char * r12mem)
 			std::cin.get();
 		}
 		//Gets byte at r3, merges it with byte at r3+1
-		r3 = ((unsigned char)r12mem[r3] << 8) | (unsigned char)r12mem[r3 + 1]; //forget this -> It's ok but compiler fucks it when using [r3+1] In addition you have to use little endian bitch
+		r3 = ((unsigned char)r12mem[r3] << 8) | (unsigned char)r12mem[r3 + 1]; 
 		r3 = _byteswap_ushort(r3); //Since nds is little endian and my PC uses big endian I'll convert it when reading from memory
 		//Unsigned int is 2 bytes -> 16 bits -> ushort
 
